629ba5f479ee0339217f5d5c961ffe8983a304cb
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-07-23 21:16:11 +0700

    update

--- src/constants/routeName.ts
+++ src/constants/routeName.ts
@@ -1,10 +1,6 @@
-import {RootStackParamList} from '../@types/navigation';
-
-const routeName: {
-  [key in keyof RootStackParamList]: string;
-} = {
-  Login: 'Login', // Use 'Login' to match the key
-  Profile: 'Profile', // Use 'Profile' to match the key
+const routeName = {
+  Login: 'Login',
+  Profile: 'Profile',
 };
 
 export {routeName};
